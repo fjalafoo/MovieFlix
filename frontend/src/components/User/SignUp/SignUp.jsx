b0f528df15555af1e6cd0ff44f2d337eee12e1e6
kawtharnaser <kawtharnasser7@gmail.com>
2022-11-14 14:10:47 +0300

    fixed signup login frontend namings and css

--- frontend/src/components/User/SignUp/SignUp.jsx
+++ frontend/src/components/User/SignUp/SignUp.jsx
@@ -47,7 +47,7 @@ function SignUp(props) {
 
 
       {/* signup page components start here  */}
-      <div className='container'>
+      <div className='signUpContainer'>
       <h1>Unlimited movies, TV shows, and more.</h1>
       <h2>Watch anywhere. Cancel anytime.</h2>
       <p>Ready to watch? Enter your email to create or restart your membership.</p>
