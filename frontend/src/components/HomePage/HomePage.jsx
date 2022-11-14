78edc8477397f8e5fcf2562c52a0867b76e6970b
kawtharnaser <kawtharnasser7@gmail.com>
2022-11-14 16:18:11 +0300

    started experimenting with the movie db api

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,8 +1,25 @@
 import NavBar from "../NavBar/NavBar";
 import "./HomePage.css";
+import axios from 'axios'
+import { useEffect } from "react";
+
 
 
 const Home = () => {
+  const grabMovies = async () =>{
+    const response  = await axios.get(`${process.env.TMBD_BASE_URL}/discover/movie`, {
+      params: {
+        API_KEY: process.env.API_KEY
+      }
+    }) 
+    console.log("The response isssssss")
+    console.log('response', response)
+
+  }
+
+  useEffect( ()=> {
+    grabMovies()
+  }, [])
   return (
     <div className="home">
       <NavBar />
