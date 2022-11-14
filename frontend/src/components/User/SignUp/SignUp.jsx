e68c6d8c76937161b58d6baf96cca62a4c328344
fjalafoo <fjalafoo@gmail.com>
2022-11-14 14:01:48 +0300

    img alt

--- frontend/src/components/User/SignUp/SignUp.jsx
+++ frontend/src/components/User/SignUp/SignUp.jsx
@@ -30,7 +30,7 @@ function SignUp(props) {
       {/* header section starts here */}
       <div className='header'>
         {/* NETFLIX logo on the left*/}
-          <img
+          <img alt=""
           className='NetflixLogo' 
           src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1597px-Netflix_2015_logo.svg.png?20190206123158'>
           </img>
