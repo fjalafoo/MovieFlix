aa372f5e7d6237e87e542f3e82b47ca9bc1e957b
Sabika Ali <Sabikaalghadeeri@gmail.com>
2022-11-14 14:05:01 +0300

    added myList controller and routes

--- backend/controllers/myList.js
+++ backend/controllers/myList.js
@@ -1,62 +1,62 @@
 // We import the Model to communicate with the Database
-// const myList = require('../models/List')
+const List = require('../models/List')
 
-// async function getAllTweets(req,res) {
-//     const allTweets = await Tweet.find()
-//     res.json(allTweets)
+async function getAllMoviesOrShows(req,res) {
+    const allMoviesOrShows = await MovieOrShow.find()
+    res.json(allMoviesOrShows)
+}
+
+async function createMoviesOrShows(req,res) {
+    console.log('Req Body!', req.body)
+    // Method 3
+    // MovieOrShow.create({
+    //     name: req.body.name,
+    //     content: req.body.content
+    // })
+    MovieOrShow.create(req.body)
+    .then(newMovieOrShow => res.json(newMovieOrShow))
+    .catch(err => res.json(err))
+}
+    // Method 2
+//     await MovieOrShow.create({
+//         name: req.body.name,
+//         content: req.body.content
+//     })
+//     // Method 1
+//     let newMovieOrShow = new MovieOrShow({
+//         name: req.body.name,
+//         content: req.body.content
+// })
+//     await newMovieOrShow.save()
 // }
 
-// async function createTweets(req,res) {
-//     console.log('Req Body!', req.body)
-//     // Method 3
-//     // Tweet.create({
-//     //     name: req.body.name,
-//     //     content: req.body.content
-//     // })
-//     Tweet.create(req.body)
-//     .then(newTweet => res.json(newTweet))
-//     .catch(err => res.json(err))
-// }
-//     // Method 2
-// //     await Tweet.create({
-// //         name: req.body.name,
-// //         content: req.body.content
-// //     })
-// //     // Method 1
-// //     let newTweet = new Tweet({
-// //         name: req.body.name,
-// //         content: req.body.content
-// // })
-// //     await newTweet.save()
-// // }
+async function updateMoviesOrShows(req, res) {
 
-// async function updateTweets(req, res) {
+    try {
+    await MovieOrShow.findByIdAndUpdate(req.params._id,req.body
+         // form of req.body {content:req.body.content})
 
-//     try {
-//     await Tweet.findByIdAndUpdate(req.params._id,req.body
-//          // form of req.body {content:req.body.content})
+    )
+    res.status(200).json({message:'MovieOrShow updated Successfuly!'})
+    // res.json(updateMovieOrShow)
+} catch(err){
+    res.json(err)
+    }
+}
 
-//     )
-//     res.status(200).json({message:'Tweet updated Successfuly!'})
-//     // res.json(updateTweet)
-// } catch(err){
-//     res.json(err)
-//     }
-// }
+async function deleteMoviesOrShows(req, res) {
+    try {
+        await MovieOrShow.findByIdAndDelete(req.params._id)
+        res.json({message: 'MovieOrShow Deleted Successfully'})
+    } catch(err) {
+        res.json(err)
+    }
+}
 
-// async function deleteTweets(req, res) {
-//     try {
-//         await Tweet.findByIdAndDelete(req.params._id)
-//         res.json({message: 'Tweet Deleted Successfully'})
-//     } catch(err) {
-//         res.json(err)
-//     }
-// }
-
-// // Exporting/Sharing our functions
-// module.exports = {
-//     getAllTweets,
-//     createTweets,
-//     updateTweets,
-//     deleteTweets
-// }
+// Exporting/Sharing our functions
+module.exports = {
+    getAllMoviesOrShows,
+    createMoviesOrShows,
+    updateMoviesOrShows,
+    deleteMoviesOrShows
+}
