a3deaf2633a4b1fb76b71c519a3fb6189d7c5f84
Nathanael Johnson <nathanealjohnson@gmail.com>
2022-11-14 14:04:21 +0300

    navbar and homepage 0.2

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
   return (
     <div className={isScrolled ? "navbar scrolled" : "navbar"}>
-      <div className="container">
+      <div className="NavBarcontainer">
         <div className="left">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
