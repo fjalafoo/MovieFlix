3be7e1e7abbf93a31200d99ea84d29e58d6e7565
fjalafoo <fjalafoo@gmail.com>
2022-11-16 21:20:59 +0300

    able to view the movies in rows

--- frontend/src/components/MovieCard/MovieCard.jsx
+++ frontend/src/components/MovieCard/MovieCard.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 function MovieCard({movie}) {
   return (
-    <div>
-      {movie.poster_path ? <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}alt=""/>
+    <div class="MovieTile">
+      {movie.poster_path ? <img class="poster" src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}alt=""/>
         :null
       }
-      <h3>{movie.title}</h3>
+      {/* <h3>{movie.title}</h3> */}
       {/* {movie.overview} */}
   
    
